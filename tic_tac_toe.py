a05f58f2ff1531129f1622d5493d4cd2ce8161f0
AbhishekMishraGit <79761978+AbhishekMishraGit@users.noreply.github.com>
2021-04-10 14:37:52 +0530

    Add files via upload

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -6,6 +6,7 @@ boardList = [' ']*10
 
 def gameboard(board):
 
+    print()
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
 
     print('-----------')
@@ -15,6 +16,7 @@ def gameboard(board):
     print('-----------')
 
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print()
 
 
 
@@ -56,39 +58,71 @@ def inputPlayerMove():
 
 def boardMove(playerLetter, playerMove):
     while True:
-        index = int(playerMove)
+        index = playerMove
         if boardList[index] == ' ':
             boardList[index] = playerLetter
             break
-     
 
+def checkIfWin(board, letter):
+    return (board[1] == letter and board[2] == letter and board[3] == letter) or (board[4] == letter and board[5] == letter and board[6] == letter) or (board[7] == letter and board[8] == letter and board[9] == letter) or (board[1] == letter and board[4] == letter and board[7] == letter) or (board[2] == letter and board[5] == letter and board[8] == letter) or (board[3] == letter and board[6] == letter and board[9] == letter) or (board[1] == letter and board[5] == letter and board[9] == letter) or (board[3] == letter and board[5] == letter and board[7] == letter) 
+
+def checkIfTie():
+    for i in range(1, 10):
+        if boardList[i] == ' ':
+            return False 
+    return True
+    
 gameboard(boardList)
 playerLetter, computerLetter = inputPlayerLetter()
-turn = 'player'#whoGoesFirst()
+turn = whoGoesFirst()
 move_Already_Played = []
 playerMove = ' '
+
 while True:
 
     if turn == 'player':
             
         while True:
-            playerMove = inputPlayerMove()
+            playerMove = int(inputPlayerMove())
             if playerMove in move_Already_Played:
                 print('Move Already Played, enter a new move from 1-9')
             else:
                 move_Already_Played.append(playerMove)
                 break
 
-    boardMove(playerLetter, playerMove)
-    gameboard(boardList)
+        boardMove(playerLetter, playerMove)
+        gameboard(boardList)
+        if checkIfWin(boardList, playerLetter):
+            print('player win')
+            break
+        if checkIfTie():
+            print('Game is Tie')
+            break
+        turn = 'computer'
 
 
 
+    if turn == 'computer':
+
+        while True:
+            computerMove = int(input('input for computer: '))
+            if computerMove in move_Already_Played:
+                print('computer ji enter another value')
+            else:
+                move_Already_Played.append(computerMove)
+                break
+
+        boardMove(computerLetter, computerMove)
+        gameboard(boardList)
+        if checkIfWin(boardList, computerLetter):
+            print('computer wins')
+            break
+        if checkIfTie():
+            print('Game is Tie')
+            break
+        turn = 'player'
 
 
 
     
         
-
-
-
